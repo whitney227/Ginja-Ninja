b876af40d56706bc29fad1ee8fa328f7815a96f1
Whitney Crawford <whitneylcrawford@gmail.com>
2019-10-02 12:24:09 -0400

    new images

--- charter/src/components/Navbar.js
+++ charter/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../images/logo-ginja-ninja.png'
+import logo from '../images/logo.png'
 import {FaAlignRight} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 
