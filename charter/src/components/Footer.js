ddacf1fc6f85a4f20613d1d2a4ed368b0f9165ec
Whitney Crawford <whitneylcrawford@gmail.com>
2019-10-04 15:24:33 -0400

    button to instagram

--- charter/src/components/Footer.js
+++ charter/src/components/Footer.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-// import {Link} from 'react-router-dom'
+
+import Card from 'react-bootstrap/Card';
 import {FaCameraRetro} from 'react-icons/fa'
 
 export default class Footer extends  Component {
     render(){
         return (
-            <nav className="footer">Connect with Us
-                <div className="">
-                    <FaCameraRetro />
-                            
-                </div>
+            <nav className="footer">
+                <p>Connect with Us</p>
+                <Card.Link target="_blank" href="https://www.instagram.com/ginja_ninja_fishingteam/">
+                    <FaCameraRetro style={{color: 'white'}}/>
+                </Card.Link>
              </nav>
         );
     }
