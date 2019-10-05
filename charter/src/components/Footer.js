c60472589f750f215e9ec927916a50901d7232db
Whitney Crawford <whitneylcrawford@gmail.com>
2019-10-05 19:44:42 -0400

    footer spacing

--- charter/src/components/Footer.js
+++ charter/src/components/Footer.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-
-import Card from 'react-bootstrap/Card';
-import {FaCameraRetro} from 'react-icons/fa'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav';
+import {FaCameraRetro, FaMobileAlt, FaEnvelope} from 'react-icons/fa'
 
 export default class Footer extends  Component {
     render(){
         return (
-            <nav className="footer">
+            <>
+            <Navbar className="footer">
                 <p>Connect with Us</p>
-                <Card.Link target="_blank" href="https://www.instagram.com/ginja_ninja_fishingteam/">
-                    <FaCameraRetro style={{color: 'white'}}/>
-                </Card.Link>
-             </nav>
+                <Nav className="mr-auto">
+                    <Nav.Link target="_blank" href="https://www.instagram.com/ginja_ninja_fishingteam/">
+                        <FaCameraRetro style={{color: 'white'}}/>
+                    </Nav.Link>
+                    <Nav.Link trigger="click" placement="top">
+                        <FaMobileAlt style={{color: 'white'}}/>
+                    </Nav.Link>
+                    <Nav.Link>
+                        <FaEnvelope style={{color: 'white'}}/>
+                    </Nav.Link>
+
+                </Nav>
+             </Navbar>
+             </>
         );
     }
 }
