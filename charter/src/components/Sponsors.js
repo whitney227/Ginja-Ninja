5cdd735a0f926fd47f25724c14d48055a64173c1
Whitney Crawford <whitneylcrawford@gmail.com>
2019-10-05 14:17:40 -0400

    trophy room photos

--- charter/src/components/Sponsors.js
+++ charter/src/components/Sponsors.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react'
-// import {Col, Container, Row, Image} from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import Title from "./Title";
@@ -16,16 +15,19 @@ export default class Sponsors extends Component {
 
                 <div className="sponsors-center">
                 <Col xs={6} md={4}>
+                    {/* eslint-disable-next-line */}
                     <a href="https://briny.com/" target="_blank">
                         <Image src="https://cdn.shopify.com/s/files/1/1129/0534/t/17/assets/logo.png?2681" />
                     </a>   
                 </Col>
                 <Col xs={6} md={4}>
+                    {/* eslint-disable-next-line */}
                     <a href="https://www.engelcoolers.com/" target="_blank">
                         <Image src="https://www.engelcoolers.com/pub/media/logo/stores/1/Engel_Coolers_Logo_3.png" />
                     </a>
                 </Col>
                 <Col xs={6} md={4}>
+                    {/* eslint-disable-next-line */}
                      <a href="https://fishrazr.com/" target="_blank">
                         <Image src={image}
                         style={{paddingTop:"0"}} />          
--- charter/src/pages/TrophyRoom.js
+++ charter/src/pages/TrophyRoom.js
@@ -1,7 +1,47 @@
 import React from 'react'
+import Title from '../components/Title'
+import Card from '../components/Card'
 import Hero from '../components/Hero'
+import CardDeck from 'react-bootstrap/CardDeck'
+import Card1 from "../images/card1.jpg"
+import Card2 from"../images/card2.jpg"
+import Card3 from"../images/card3.jpg"
+import Card4 from"../images/card4.JPG"
+import Card5 from"../images/card5.jpg"
+import Card6 from"../images/card6.JPG"
+import Card7 from "../images/card7.jpg"
+import Card8 from "../images/card8.jpg"
+import Card9 from "../images/card9.JPG"
+import Card10 from "../images/card10.jpg"
+import Card11 from "../images/card11.jpg"
+import Card12 from "../images/card12.JPG"
 
 const TrophyRoom = () => {
-    return <Hero hero="trophyHero" />;
+    return (
+        <>
+        <Hero hero="trophyHero" />
+        <br></br>
+        <Title title="Photo Gallery" />
+        <CardDeck className="cardDeck">
+            <Card image={Card1} />
+            <Card image={Card2} />
+            <Card image={Card3} />
+            <Card image={Card4} />
+        </CardDeck>
+        <CardDeck className="cardDeck">
+            <Card image={Card5} />
+            <Card image={Card6} />
+            <Card image={Card7} />
+            <Card image={Card8} />
+        </CardDeck>
+        <CardDeck className="cardDeck">
+            <Card image={Card9} />
+            <Card image={Card10} />
+            <Card image={Card11} />
+            <Card image={Card12} />
+        </CardDeck>
+        <br></br>
+        </>
+    );
 };
 export default TrophyRoom
