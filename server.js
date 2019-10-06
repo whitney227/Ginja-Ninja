d7ed07a493de38514b529e3669fe1b709f59ab5b
Whitney Crawford <whitneylcrawford@gmail.com>
2019-10-06 15:13:59 -0400

    changes to express server

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
