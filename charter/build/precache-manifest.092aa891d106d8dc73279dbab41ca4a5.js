27ca1008dd5d9f7dd45ba463ac8f65703cf32a79
Whitney Crawford <whitneylcrawford@gmail.com>
2019-10-06 21:06:51 -0400

    build no longer ignored

---
+++ charter/build/precache-manifest.092aa891d106d8dc73279dbab41ca4a5.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1086f692ab3000c898cdb3cd6df742d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edc59fd927bb7512aed3",
+    "url": "/static/css/main.ae14c2bf.chunk.css"
+  },
+  {
+    "revision": "4ae16be436b5f47b1683",
+    "url": "/static/js/2.c978e678.chunk.js"
+  },
+  {
+    "revision": "edc59fd927bb7512aed3",
+    "url": "/static/js/main.b820276e.chunk.js"
+  },
+  {
+    "revision": "96a9d6d34e38486a825a",
+    "url": "/static/js/runtime-main.5f4e9bdd.js"
+  },
+  {
+    "revision": "35291593592ac1adb4afb96118c083fe",
+    "url": "/static/media/card1.35291593.jpg"
+  },
+  {
+    "revision": "6e7871e8e350c8398279ea7e44f4144a",
+    "url": "/static/media/card10.6e7871e8.jpg"
+  },
+  {
+    "revision": "71f1b4b6fc3f976d9e16aabcf5567aac",
+    "url": "/static/media/card11.71f1b4b6.jpg"
+  },
+  {
+    "revision": "fdff24d0b4ce911cc9c4d7ee005a6ade",
+    "url": "/static/media/card12.fdff24d0.JPG"
+  },
+  {
+    "revision": "f18fc489b2de532049f42fae191ca555",
+    "url": "/static/media/card13.f18fc489.JPG"
+  },
+  {
+    "revision": "5cefa9c8f8f259598c5eaef50d152e10",
+    "url": "/static/media/card14.5cefa9c8.jpg"
+  },
+  {
+    "revision": "2701fe37bef111051fe9ff192521f90b",
+    "url": "/static/media/card15.2701fe37.jpg"
+  },
+  {
+    "revision": "6bdc60d13545cb14e258033195d17816",
+    "url": "/static/media/card16.6bdc60d1.png"
+  },
+  {
+    "revision": "87591e76d61d1b44bf5d976383ae2e73",
+    "url": "/static/media/card2.87591e76.jpg"
+  },
+  {
+    "revision": "b40e263df648066541476054b36214a9",
+    "url": "/static/media/card3.b40e263d.jpg"
+  },
+  {
+    "revision": "12450935bc749ffe242eac26c8831e32",
+    "url": "/static/media/card4.12450935.JPG"
+  },
+  {
+    "revision": "343116b4b98614ad522e3f3e7a53e5ff",
+    "url": "/static/media/card5.343116b4.jpg"
+  },
+  {
+    "revision": "56672bf6167b20955a60a75cbbc5c769",
+    "url": "/static/media/card6.56672bf6.JPG"
+  },
+  {
+    "revision": "01cc9160d67aa48851550b1cdef9466c",
+    "url": "/static/media/card7.01cc9160.jpg"
+  },
+  {
+    "revision": "03fe969ec1e2124dbc86f58ed3272762",
+    "url": "/static/media/card8.03fe969e.jpg"
+  },
+  {
+    "revision": "9dac9d76358d61f57964fd854a8c4b8a",
+    "url": "/static/media/card9.9dac9d76.JPG"
+  },
+  {
+    "revision": "ef02cd1f0d28cae398aeef11db5e5f3d",
+    "url": "/static/media/default-header.ef02cd1f.JPG"
+  },
+  {
+    "revision": "68cbc69b4b245f52892c712b2aeac910",
+    "url": "/static/media/fishrzr.68cbc69b.jpg"
+  },
+  {
+    "revision": "a7f27a6cae5381324a8fff9ecfd104ef",
+    "url": "/static/media/logo.a7f27a6c.png"
+  },
+  {
+    "revision": "a20d691fc8062b8048f414deb4df4045",
+    "url": "/static/media/packages-hero3.a20d691f.jpg"
+  },
+  {
+    "revision": "5336195373c16b396a0868eff0012225",
+    "url": "/static/media/trophy-room-hero.53361953.png"
+  }
+]);
