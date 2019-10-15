774850836531ed9a17043be246b6ac96e50d1ac3
Whitney Crawford <whitneylcrawford@gmail.com>
2019-10-15 17:19:15 -0400

    tooltip for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav';
 import {FaCameraRetro, FaMobileAlt, FaEnvelope, FaRegCopyright} from 'react-icons/fa'
+import ReactHintFactory from 'react-hint'
+const ReactHint = ReactHintFactory(React)
 
 export default class Footer extends  Component {
     render(){
@@ -10,14 +12,17 @@ export default class Footer extends  Component {
             <Navbar className="footer">
                 <p>Connect with Us</p>
                 <Nav className="mr-auto">
+                    <ReactHint className="tooltip" persist="true"
+                    events={{click: true, focus: true}} />
+
                     <Nav.Link target="_blank" href="https://www.instagram.com/ginja_ninja_fishingteam/">
                         <FaCameraRetro style={{color: 'white'}}/>
                     </Nav.Link>
-                    <Nav.Item>
-                        <FaMobileAlt style={{color: 'white'}}/>
+                    <Nav.Item data-rh="803-924-5817" data-rh-at="bottom">
+                        <FaMobileAlt style={{color: 'white'}} />    
                     </Nav.Item>
-                    <Nav.Item>
-                        <FaEnvelope style={{color: 'white'}}/>
+                    <Nav.Item data-rh="trippamick@yahoo.com" data-rh-at="bottom">
+                        <FaEnvelope style={{color: 'white'}} />
                     </Nav.Item>
                 </Nav>
                 <p style={{fontSize:"18px", paddingTop:"10px"}}> 
